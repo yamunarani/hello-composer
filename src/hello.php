4459314547ee6e7aad1e2c3fb522f4da959d7415
yamunarani <yamunsk@yahoo.co.in>
2021-01-07 22:57:31 +0530

    version 3

--- src/hello.php
+++ src/hello.php
@@ -9,7 +9,7 @@
 
 // If you don't to add a custom vendor folder, then use the simple class
 // namespace HelloComposer;
-namespace ourcodeworld\HelloComposer;
+namespace yamunarani\HelloComposer;
 
 class Hello
 {
